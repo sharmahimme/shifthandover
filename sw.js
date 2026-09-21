807c6c022285ea8d1fbbe8183478d30decf64463
sharmahimme <sharmahimme@gmail.com>
2026-09-21 19:03:24 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='hkstock-v4';
-const ASSETS=['./','./index.html','./manifest.json'];
+const CACHE='hkstock-v5';
+const ASSETS=['./','./index.html','./manifest.json','./qrcode.min.js','./jsQR.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(resp=>{const cp=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return resp;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
